11f2667d933bca618e4d373ee28a40f8cffa92f2
krungy <furler711@gmail.com>
2024-02-10 19:01:26 +0900

    [func] 로그인 모달 구현

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,3 +1,8 @@
+
 export default function Profile() {
-  return <main>프로필</main>;
+  return (
+    <main>
+      프로필
+    </main>
+  )
 }
--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <main>게시글 상세</main>;
+  return (
+    <main>
+      게시글 상세
+    </main>
+  )
 }
